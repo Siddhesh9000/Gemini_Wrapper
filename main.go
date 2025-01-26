c0556506cd9fdfd62ec7d185649bfc9d00c85ce5
Siddhesh9000 <98513953+Siddhesh9000@users.noreply.github.com>
2025-01-26 10:51:12 +0530

    Update main.go

--- main.go
+++ main.go
@@ -15,14 +15,5 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-
-// Check for Vercel environment (only used in Vercel deployment)
-isVercel := os.Getenv("VERCEL")
-if isVercel != "" {
-	// Skip local server in Vercel environment, handle via generate.go
-	log.Println("Running in Vercel, using generate.go")
-} else {
-	// Start the local server (for development)
 	server.StartServer()
-}
 }
